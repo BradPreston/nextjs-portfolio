76ec7619ea058f11ff25724fedaeaf5e38516145
bradpreston <bap5393@gmail.com>
2024-04-21 21:29:27 -0400

    Format root layout and set background to plum

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import './globals.css';
 
 const poppins = Poppins({ weight: '400', subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Brad Preston",
-  description: "My professional portfolio showcasing my recent projects, blog posts, and accomplishments.",
+	title: 'Brad Preston',
+	description: 'My professional portfolio showcasing my recent projects, blog posts, and accomplishments.'
 };
 
 export default function RootLayout({
-  children,
+	children
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={`${poppins.className} bg-plum`}>{children}</body>
+		</html>
+	);
 }
