2dbe3c936c483cd3e7c3e6bff32daf6968839aa2
bradpreston <bap5393@gmail.com>
2024-04-21 21:28:35 -0400

    Update custom colors in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		colors: {
+			plum: 'rgb(43, 42, 52)',
+			gold: 'rgb(203, 167, 47)',
+			salmon: {
+				DEFAULT: 'rgb(233, 128, 120)',
+				light: 'rgba(213, 99, 90, 0.25)'
+			},
+			white: 'rgb(255, 255, 255)'
+		}
+	},
+	plugins: []
 };
 export default config;
